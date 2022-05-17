36429195d32b0a7341852c972aac4a1c8313d81e
Juliana Sato <julianalumi96@gmail.com>
2022-05-17 20:59:08 -0300

    commit header file

---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jusato <jusato@student.42sp.org.br>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/04 23:26:50 by jusato            #+#    #+#             */
+/*   Updated: 2022/05/18 00:31:49 by jusato           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+// #ifndef BUFFER_SIZE
+// # define BUFFER_SIZE 10
+// #endif
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *str, int c);
+
+#endif
